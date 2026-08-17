c8e51589351ce8b6a7637b92686b28fa8423b748
Khun <khunly@bstorm.be>
2026-08-17 13:11:28 +0200

    init

---
+++ addons/demo/__manifest__.py
@@ -0,0 +1,13 @@
+{  # noqa: B018
+    "name": "Demo Technifutur",
+    "version": "1.0.0",
+    "description": """
+        Test module for Technifutur
+    """,
+    "depends": ["base"],
+    "author": "Khun LY",
+    "data": [],
+    "category": "administration",
+    "application": True,
+    "installable": True
+}
